73ca45eac8b6ff24d3da6688ffd0cf68bfb23466
Mariusz Wnek <mariusz@theatrejs.com>
2022-04-20 10:16:30 +0200

    Change define plugin

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -44,7 +44,9 @@ module.exports = {
   },
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production'),
+      'process.env.NODE_ENV': JSON.stringify(
+        process.env.NODE_ENV || 'development'
+      ),
     }),
     new MiniCssExtractPugin({
       filename: '[name].[contenthash].css',
